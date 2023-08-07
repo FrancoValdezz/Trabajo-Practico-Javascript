6f6ffb424e0e796fa4f4913be1b3631d5a27fae8
Franco Valdez <francovaldezut@gmail.com>
2023-08-07 20:48:00 -0300

    3ra entrega

--- main.js
+++ main.js
@@ -1,118 +1,84 @@
-class Jugador {
-    constructor(nombre, mesa, dinero, tiradas) {
-        this.nombre = nombre;
-        this.mesa = mesa;
-        this.dinero = dinero;
-        this.tiradas = tiradas;
+const btnTirar = document.getElementById('tirar')
+const dadoCara = document.getElementById('dadoCara')
+const numeroCalienteParrafo = document.getElementById('numeroCaliente');
+const numeroFrioParrafo = document.getElementById('numeroFrio');
+const loginForm = document.getElementById('loginForm');
+const conteinergame = document.getElementById('conteiner');
+const conteinerform = document.getElementsByClassName('login-container')
+const numeroElegidoInput = document.getElementById('numeroElegido');
+const hiteo = document.getElementById('hit');
+
+function iniciarJuego(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const dinero = parseFloat(document.getElementById('dinero').value);
+
+    //creo un objeto usuario
+    const usuario = {
+        nombre: nombre,
+        dinero: dinero
+    };
+    // guardo el objeto en el LocalStorage para usarlo mas adelante
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+    //NO OLVIDARME DE UTILIZAR EL DINERO!!!
+    
+    loginForm.style.display = 'none';
+    conteinergame.style.display = 'block';
+}  
+
+loginForm.addEventListener('submit', iniciarJuego);
+
+
+
+//Creo un array con las imagenes para poder mostrar el lado del dado
+//que ha salido
+
+const lados = [
+    'img/lado1.png',
+    'img/lado2.png',
+    'img/lado3.png',
+    'img/lado4.png',
+    'img/lado5.png',
+    'img/lado6.png',]
+
+
+// Creo un array que contiene las veces que sale cada lado
+const ladosQueSalieron = [0,0,0,0,0,0]
+
+function tirarDado() {
+    const resultado = Math.floor(Math.random() * 6)
+
+    dadoCara.src = lados[resultado]
+    //hace que aparesca la imagen
+    dadoCara.style.display = 'inline'
+    ladosQueSalieron[resultado]++
+
+    //calcular los que salieron mas veces y menos veces
+    const maxApariciones = Math.max(...ladosQueSalieron);
+    const minApariciones = Math.min(...ladosQueSalieron);
+
+    const numeroCaliente = ladosQueSalieron.indexOf(maxApariciones) + 1;
+    const numeroFrio = ladosQueSalieron.indexOf(minApariciones) + 1;
+
+    numeroCalienteParrafo.textContent = `Número caliente: ${numeroCaliente}`;
+    numeroFrioParrafo.textContent = `Número frío: ${numeroFrio}`;
+
+
+    //aca me fijo si hiteo
+    const miNumero = parseInt(numeroElegidoInput.value)
+
+    if  (miNumero === (resultado + 1)) {
+        hiteo.textContent = 'Has hiteado';
+    } else {
+        hiteo.textContent = 'No hiteaste!';
     }
+
+    console.log(ladosQueSalieron)
 
 }
 
-function cargar_jugadores(jugadores, cantidad_jugadores) {
-    for (let i = 0; i < cantidad_jugadores; i++) {
-        const nombre = 'Jugador ' + i
-        const mesa = Math.floor(Math.random() * 6)
-        const dinero = Math.floor(Math.random() * 500)
-        //Genero el array dentro del objeto//
-        const tiradas = [];
-        for (let j = 0; j < 6; j++) {
-            const tirada = Math.floor(Math.random() * 10) + 1; 
-            tiradas.push(tirada);
-            //Aca lo que hago es hacer un ciclo for para que en cada vuelta tome un valor entre 0 y 6 y lo agregue al array simulando los lados de la cara del dado por ejemplo: *se genera el numero 4 entonces seria que salio 4 veces entonces lo agregaria en el array[0] que simularia que es la cara del dado 1*//
-        }
-    const jugador = new Jugador(nombre, mesa, dinero, tiradas);
-    jugadores.push(jugador);
-    }
-}
-
-function menu(){
-    console.log('1. MOSTRAR JUGADORES')
-    console.log('2. BUSCAR EL QUE TIENE MAS DINEROO')
-    console.log('3. SALIR')
-}
-
-function mostrarJugadores(jugadores) {
-    console.log("Jugadores:");
-    jugadores.forEach((jugador) => {
-    console.log(jugador.nombre + '\n'+ 'Mesa Nº: ' +jugador.mesa +'\n' + 'Dinero del jugador' + jugador.dinero + '\n' + 'Cantidad de veces que salio el lado del dado: ' +jugador.tiradas);
-    });
-}
-
-function buscarMayorDinero(jugadores){
-    // le doy el mayor al primer jugador porque si no se ingresa ninguno este seria el mayor y seria el primero en compararse con el que esta siguente a el
-    let mayor = jugadores[0]
-    for(let i = 1; i < jugadores.length ; i++){
-        if(mayor.dinero < jugadores[i].dinero){
-            mayor = jugadores[i]
-        }
-    }
-    return mayor
-}
-
-
-const jugadores = [];
-const cantidad_jugadores = parseInt(prompt('Cargar los jugadores de las mesas'));
-
-cargar_jugadores(jugadores, cantidad_jugadores);
-
-let opcion = ''
-//Aca voy a hacer un tipo "menu" para que eligir una opcion
-do {
-    menu();
-    opcion = parseInt(prompt('Ingrese el número de la opción que desea:'));
-    switch (opcion) {
-        case 1:
-            mostrarJugadores(jugadores);
-            break;
-        case 2:
-            mayorDinero = buscarMayorDinero(jugadores)
-            console.log('El que mas dinero tiene es' + mayorDinero.nombre + 'con ' + mayorDinero.dinero)
-        case 3:
-            console.log("Ha salido del numeroo"); 
-            break;
-        default:
-            console.log("ERROR. elegir numero entre 1 y 3");
-    }
-} while (opcion !== 3);
-
-
-
-
-
-/* function mayorQue(mensaje){
-    let ladoCara = parseInt(prompt(mensaje));
-    while(ladoCara <0 || ladoCara > 6){ 
-        alert('ERROR EL DADO SOLO TIENE 6 CARAS DEL 1 AL 6')
-        ladoCara = prompt(mensaje);
-    }
-    return ladoCara
-}
-
-
-function comprobarQue(){
-    let hit = false
-    let veces = 0
-    for (let i = 0; i < cantidad; i++){
-        var dado = Math.floor(Math.random()*6) + 1
-        console.log('Valor del dado dados: ' + dado)
-        if(dado === cara){
-            hit = true
-            veces += 1 
-        }
-    }
-    if(hit){
-        alert('Hiteaste ' + veces + ' veces')
-    }
-    else{
-        alert('No hiteaste')
-    }
-} */
-
-/* alert('Elegir una cara del dado')
-const cantidad= prompt('Cuantas veces quieres tirar el dado?');
-let cara = mayorQue('Cara:')
-comprobarQue()
-console.log('La cara que elegiste: ' + cara) */
+btnTirar.addEventListener('click',tirarDado)
 
 
 
